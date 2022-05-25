5fe2d6819f04389e0edb653379050a1f9354596e
GrincenkoDaria <grincenkodaria878@gmail.com>
2022-05-25 15:47:00 +0200

    1lesson next

--- 1les.py
+++ 1les.py
@@ -1,3 +1,13 @@
 n=5
-string="Hello World "
+string="Ahoj  "
 print(string * n)
+
+name=input("Ako sa valas? ")
+print(name,", Ako sa mas?")
+input()
+
+x=input("Cislo x je ")
+y=input("Cislo y je ")
+if x > y:
+  print("Cislo x viac")
+else: print("Cislo y viac")
